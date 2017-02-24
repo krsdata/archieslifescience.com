a0fd8fa9500d41c40b8667a5a7d84d6cc9ac5987
root <root@host.infowayindia.in>
2017-02-24 12:52:26 +0000

    backup

---
+++ haematanic.php
@@ -0,0 +1,134 @@
+﻿<!DOCTYPE HTML> <!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en-us"><![endif]--> <!--[if IE 7]><html class="no-js lt-ie9 lt-ie8 ie7" lang="en-us"><![endif]--> <!--[if IE 8]><html class="no-js lt-ie9 ie8" lang="en-us"><![endif]--> <!--[if gt IE 8]><html class="no-js ie9" lang="en-us"><![endif]-->
+<html lang="en-us" class="default" >
+<head>
+<meta charset="utf-8" />
+<title>Archies</title>
+<meta name="description" content="Archies Life Science" />
+<meta name="generator" content="Archies Life Science" />
+<meta name="robots" content="index,follow" />
+<meta name="viewport" content="width=device-width, minimum-scale=0.25, maximum-scale=1.6, initial-scale=1.0" />
+<meta name="apple-mobile-web-app-capable" content="yes" /><link rel="icon" type="image/vnd.microsoft.icon" href="#" />
+<style>
+	.none
+	{
+	display:none;
+	}
+	.text-center.col-lg-6.col-md-6.col-sm-6.col-xs-12.col-sp-12.ApColumn 
+	{
+	  float: right;
+	}
+	.price , .old-price
+	{
+		display:none !important;
+	}
+	#best-sellers_block_right img
+	{
+		width:80px;
+	}
+	.paneltool
+	{
+		display:none;
+	}
+	img
+	{
+	max-width:100%;
+	}
+	.product_img img
+	{
+	margin-bottom:15px;
+	}
+
+</style>
+<link rel="shortcut icon" type="image/x-icon" href="" />
+<link rel="stylesheet" href="assets/css/MS_880.css" type="text/css" media="all" />
+<link rel="stylesheet" type="text/css" href="assets/css/index44.css"/>
+<link rel="stylesheet" type="text/css" href="assets/css/index46.css"/> 
+<link rel="stylesheet" href="assets/css/MS_88.css" type="text/css" media="all" />
+<link rel="stylesheet" href="assets/css/index6.css" type="text/css" media="all" />
+<link href='assets/css/MS_3.css' rel='stylesheet' type='text/css'><link href='assets/css/MS_2.css' rel='stylesheet' type='text/css'> <!--[if IE 8]> <script src="#//oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script> <script src="#//oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script> <![endif]-->
+<link rel="stylesheet" href="assets/font-awesome.min.css" type="text/css" media="all" />
+</head>
+
+<body id="cms" class="cms cms-4 cms-about-us hide-left-column hide-right-column lang_en fullwidth double-menu"> <section id="page" data-column="col-xs-12 col-sm-12 col-md-4" data-type="grid"> 
+
+<header id="header" class="header-center">
+<section class="header-container">
+<div id="header-main" class="header-left">
+<div id="header_content">
+<div class="inner">
+<div id="topbar">
+<div class="container">
+<div style="" class="row ApRow ">
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 col-sp-12 ApColumn ">
+<div class="header_user_info pull-right e-translate-top">
+<div class="popup-title" data-toggle="dropdown">
+<i class="fa fa-cog"></i><span>Top links </span></div>
+<ul class="links">
+<li><a class="login" href="#" rel="nofollow" title="Login to your customer account"> <i class="fa fa-unlock-alt"></i>Email: archieslifescience@gmail.com</a></li>
+<li> <a href="#" title="My account"><i class="fa fa-user"></i>Call Us: 8818009361 / 62 / 63 / 64 / 65</a></li></ul></div>
+
+
+
+
+</div></div></div></div>
+
+<div class="top_header"><div class="container"><div class="wrapper" ><div class="header-top"><div class="row ApRow has-bg bg-boxed" data-bg=" no-repeat" style="background: no-repeat;margin-top: 30px;margin-bottom: 30px;" ><div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 col-sp-12 ApColumn " > <a href="index.php" title="Archies Life Science"> <img class="logo img-responsive" src="img/logo.jpg" alt="leo HealthyCare" width="200" height="53"/> </a></div><div class="text-center col-lg-6 col-md-6 col-sm-6 col-xs-12 col-sp-12 ApColumn " ><div id="search_block_top"><form id="searchbox" class="clearfix" method="get" action="#" > <input type="hidden" name="controller" value="search" /> <input type="hidden" name="orderby" value="position" /> <input type="hidden" name="orderway" value="desc" /> <input class="search_query form-control" type="text" name="search_query" placeholder="Search" value="" /> <button type="submit" class="btn btn-outline-inverse fa fa-search">&nbsp;</button></form></div></div>
+</div></div></div><div class="wrapper" ><div class="header-bot"><div class="row ApRow has-bg bg-boxed" data-bg=" no-repeat" style="background: no-repeat;" ><div class="menu-static col-lg-12 col-md-12 col-sm-12 col-xs-12 col-sp-12 ApColumn " ><div id="leo-megamenu" class="clearfix"> <nav id="cavas_menu" class="sf-contener leo-megamenu"><div class="" role="navigation"><div class="navbar-header"> <button type="button" class="navbar-toggle btn-outline-inverse" data-toggle="collapse" data-target=".navbar-ex1-collapse"> <span class="sr-only">Toggle navigation</span> <span class="fa fa-bars"></span> </button></div><div id="leo-top-menu" class="collapse navbar-collapse navbar-ex1-collapse">
+<ul class="nav navbar-nav megamenu">
+<li class="parent dropdown home " ><a class="dropdown-toggle has-category" data-toggle="dropdown" href="index.php" target="_self"><span class="menu-title">Home</span></a></li>
+
+<li class="parent dropdown home " ><a class="dropdown-toggle has-category" data-toggle="dropdown" href="#" target="_self"><span class="menu-title">About Us</span><b class="caret"></b></a><div 	class="dropdown-menu level1" ><div class="dropdown-menu-inner"><div class="row"><div class="mega-col col-sm-12" data-type="menu" ><div class="mega-col-inner "><ul><li class=" " ><a href="4-about-us.php"><span class="menu-title">Company Profile</span></a></li><li class=" " ><a href="vision.php"><span class="menu-title">Vision & Mission</span></a></li><li class=" " ><a href="our_location.php"><span class="menu-title">Our Location</span></a></li></ul></div></div></div></div></div></li>
+
+<li class="parent dropdown home " ><a class="dropdown-toggle has-category" data-toggle="dropdown" href="#" target="_self"><span class="menu-title">Products</span><b class="caret"></b></a><div class="dropdown-menu level1" ><div class="dropdown-menu-inner"><div class="row"><div class="mega-col col-sm-12" data-type="menu" ><div class="mega-col-inner "><ul><li class=" "><a href="antibiotic.php"><span class="menu-title"> ANTIBIOTIC </span></a></li><li class=" "><a href="neuropathic.php"><span class="menu-title"> NEUROPATHIC  </span></a></li><li class=" "><a href="antacid.php"><span class="menu-title"> ANTACID  </span></a></li>
+<li class=" "><a href="muscle_relaxant.php"><span class="menu-title"> MUSCLE RELAXANT  </span></a></li>
+<li class=" "><a href="antihelminthic.php"><span class="menu-title"> ANTIHELMINTHIC  </span></a></li><li class=" "><a href="analgesic.php"><span class="menu-title"> ANALGESIC </span></a></li><li class=" "><a href="antifungal.php"><span class="menu-title"> ANTIFUNGAL </span></a></li><li class=" "><a href="haematanic.php"><span class="menu-title"> HAEMATANIC </span></a></li><li class=" "><a href="steroid.php"><span class="menu-title"> STEROID </span></a></li><li class=" "><a href="multivitamins.php"><span class="menu-title"> MULTIVITAMIN </span></a></li><li class=" "><a href="calcium_supplement.php"><span class="menu-title"> CALCIUM SUPPLEMENT </span></a></li><li class=" "><a href="appetizer.php"><span class="menu-title"> APPETIZER </span></a></li><li class=" "><a href="cough_formulation.php"><span class="menu-title"> COUGH FORMULATION </span></a></li><li class=" "><a href="protein_supplemen.php"><span class="menu-title"> PROTEIN SUPPLEMENT  </span></a></li><li class=" "><a href="metabolic.php"><span class="menu-title"> METABOLIC </span></a></li><li class=" "><a href="portal_hypertension.php"><span class="menu-title"> PORTAL HYPERTENSION </span></a></li></ul></div></div></div></div></div></li>
+
+<li class="parent dropdown home " ><a class="dropdown-toggle has-category" data-toggle="dropdown" href="#" target="_self"><span class="menu-title">Franchise</span><b class="caret"></b></a><div class="dropdown-menu level1" ><div class="dropdown-menu-inner"><div class="row"><div class="mega-col col-sm-12" data-type="menu" ><div class="mega-col-inner "><ul><li class=" " ><a href="franchis_register.php"><span class="menu-title">Registration For Franchise</span></a></li><li class=" " ><a href="franchis_bulk.php"><span class="menu-title">Registration For Bulk Order</span></a></li></ul></div></div></div></div></div></li>
+
+<li class="parent dropdown home " ><a class="dropdown-toggle has-category" data-toggle="dropdown" href="make_in_india.php" target="_self"><span class="menu-title">Make In India</span></a></li>
+
+
+
+<li class="parent dropdown home " ><a class="dropdown-toggle has-category" data-toggle="dropdown" href="feedback.php" target="_self"><span class="menu-title">Feedback</span></a></li>
+
+<li class="parent dropdown home " ><a class="dropdown-toggle has-category" data-toggle="dropdown" href="career.php" target="_self"><span class="menu-title">career</span></a></li>
+
+
+<li class="parent dropdown home " ><a class="dropdown-toggle has-category" data-toggle="dropdown" href="contact-us.php" target="_self"><span class="menu-title">Contact Us</span></a></li>
+
+
+</ul></div></div> </nav></div> </div></div></div></div></div></div>
+
+</div></div></div> </section> 
+</header>
+
+<div id="breadcrumb" class="clearfix">
+<div class="container">
+	<div class="inner_banner">
+		<img style="margin-top:15px;" alt="banner" src="img/about.jpg">
+	</div>
+</div>
+
+<div class="container"><div class="breadcrumb clearfix"><div class="breadcrumb-ct"> <a class="home" href="../index.php" title="Return to Home">Home</a> <span class="navigation-pipe">&gt;</span> Products</div></div></div>
+
+
+
+
+</div> <section id="columns" class="columns-container product_img"><div class="container"><div class="row"> <section id="center_column" class="col-md-12"><div class="rte"><h1 class="page-heading bottom-indent">OFFERED HAEMATANIC PRODUCTS</h1>
+<div class="row">
+	
+</div>
+
+
+
+
+
+</div> <br /> </section></div></div> </section> 
+
+<footer id="footer" class="footer-container"><div class="container"><div id="form_14442057995543898" class="row top_footer ApRow has-bg bg-fullwidth" data-bg=" #6abf61 no-repeat" style="" ><div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 col-sp-12 ApColumn " ><div id="image-form_7321585041210170" class="block logo-footer ApImage"> <a href="#" > <img src="img/footer_logo.png" class="img-responsive " title="" alt="logo footer" style=" width:auto; height:auto" /></a></div></div><div class="text-about col-lg-6 col-md-6 col-sm-6 col-xs-12 col-sp-12 ApColumn " ><div class="ApHtml block"><h4 class="title_block">about us</h4><p>We aim to improve people's quality of life with our products.
+ To Achieve this, we concentrate on the research and development of innovative drugs and novel therapeutic approaches. 
+At the same time, we are constantly improving established products.</p></div></div><div class="block col-lg-3 col-md-3 col-sm-3 col-xs-12 col-sp-12 ApColumn " > 
+<section id="social_block"><h4 class="title_block">Follow us</h4><ul><li class="facebook"> <a target="_blank" href="https://www.facebook.com/archies.lifescience" class="_blank"> <span>Facebook</span> </a></li><li class="twitter"> <a target="_blank" href="https://twitter.com/archieslife1" class="_blank"> <span>Twitter</span> </a></li></ul> </section><div class="clearfix"></div></div></div>
+<div class="row ApRow has-bg bg-boxed" data-bg=" no-repeat" style="background: no-repeat;padding-top: 40px;padding-bottom: 40px;" ><div class="footer_about col-lg-3 col-md-3 col-sm-6 col-xs-12 col-sp-12 ApColumn " ><div class="ApRawHtml block"><h4 class="title_block">opening hours</h4><div class="aboutus"><em> Monday - Saturday ...... 9:30am -6:00pm</em> <p>Sunday is off</p></div></div></div><div class="footer_about col-lg-2 col-md-2 col-sm-6 col-xs-12 col-sp-12 ApColumn " ><div class="footer-block block" id="block_various_links_footer"><h4 class="title_block">Information</h4><ul class="toggle-footer list-group bullet"><li class="item"> <a href="index.php" title=""> Best sellers </a></li><li class="item"> <a href="contact-us.php" title="Contact us"> Contact us </a></li><li class="item"> <a href="4-about-us.php" title="About us"> About us </a></li><li> <a href="make_in_india.php" title="make in india"> Make In India </a></li></ul></div></div><div class="footer_about col-lg-2 col-md-2 col-sm-6 col-xs-12 col-sp-12 ApColumn " ><div id="blockLink-form_1442319894" class="ApBlockLink"><div class="block footer-block"><h4 class="title_block">support</h4><ul class="toggle-footer list-group bullet"><li><a href="faq.php">F.A.Q</a></li><li><a href="t&c.php"> Term &amp; Condition</a></li><li><a href="contact-us.php"> Contact Us</a></li><li><a href="franchis_register.php"> Get Started</a></li></ul></div></div></div><div class="footer_about col-lg-2 col-md-2 col-sm-6 col-xs-12 col-sp-12 ApColumn " ><div class="footer-block block"><h4 class="title_block"><a href="#" title="" rel="nofollow">Important Links</a></h4><div class="block_content toggle-footer"><ul class="bullet toggle-footer list-group"><li><a href="franchis_register.php" title="Franchise" rel="nofollow">Franchise</a></li><li><a href="career.php" title="" rel="nofollow">Career</a></li><li><a href="feedback.php" title="" rel="nofollow">Feedback</a></li></ul></div></div></div><div class="footer_about col-lg-3 col-md-3 col-sm-6 col-xs-12 col-sp-12 ApColumn " ><div class="ApRawHtml block"><h4 class="title_block">contact info</h4><div class="contacts"><a href="contact-us.php"><em class="fa fa-map-marker"></em>Ward No 1 , Bharampuri Colony , Hussani Road , Naraiangarh , Ambala Hariyana Pin code - 134203 </a> <a href="#"><em class="fa fa-send"></em>archieslifescience@gmail.com</a></div></div></div></div><div id="form_7850703349969259" class="row footer_bottom ApRow has-bg bg-fullwidth" data-bg=" #f2f2f2transparent no-repeat" style="padding-top: 20px;padding-bottom: 20px;" ><div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 col-sp-12 ApColumn " ><div class="ApHtml block"><div>© 2016 Archies Life Science. All Rights Reserved. Designed By ANV Infosystem</div></div></div><div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 col-sp-12 ApColumn " ><div id="newsletter_block_left" class="block inline"><h4 class="title_block">newsletter</h4><div class="block_content"><form action="" method="post"><div class="form-group" >  <input type="hidden" name="action" value="0" /></div></form></div></div></div></div></div> </footer> 
+
+ </section></script></body></html>
